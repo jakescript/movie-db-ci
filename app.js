9dacb88e2c2dce0a28e4aafd6ea1474fd7ef83ab
Jacob Goldstein <by.jakeg@gmail.com>
2021-02-01 20:37:21 -0500

    added /api/movies route

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express")
 const app = express()
+const {models: {Movie} } = require("./db")
 
 app.get("/", (req,res)=>{
-  res.status(200).send({msg: "success"})
+  res.status(200).send({
+    movies: "/api/movies"
+  })
+})
+
+app.get("/api/movies", async(req,res,next)=>{
+  try {
+    res.status(200).send(await Movie.findAll())
+  } catch (ex) {
+    next(ex)
+  }
+
 })
 
 module.exports = app
--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,19 @@
 const { expect } = require("chai")
+const {syncAndSeed} = require("../db")
+
 const app = require("supertest")(require("../app"))
 describe("Routes", ()=>{
+  before(()=> syncAndSeed())
+
   describe("GET /", ()=>{
     it("shows available endpoints", async()=>{
       const resp = await app.get("/")
+      expect(resp.status).to.equal(200)
+    })
+  })
+  describe("GET /api/movies", ()=>{
+    it("returns an array of movies", async()=>{
+      const resp = await app.get("/api/movies")
       expect(resp.status).to.equal(200)
     })
   })
